a7de62cac21e11bc43a775f8d43c60febd84565d
Nukeechka <nukeechka@gmail.com>
2025-01-01 19:16:17 +0500

    Add insert & delete methods for class Tree

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,8 +4,10 @@ require_relative 'node'
 
 # class Tree
 class Tree
+  attr_accessor :root
+
   def initialize(array)
-    @array = array.uniq.sort!
+    @array = array.uniq!.sort!
     @root = root_set(array)
   end
 
@@ -30,5 +32,43 @@ class Tree
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value, root = @root)
+    return Node.new(value) if root.nil?
+
+    return root if root.data == value
+
+    if root.data < value
+      root.right = insert(value, root.right)
+    else
+      root.left = insert(value, root.left)
+    end
+    root
+  end
+
+  def get_succesor(current)
+    current = current.right
+    current = current.left while !current.nil? && !current.left.nil?
+    current
+  end
+
+  def delete(value, root = @root) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    return root if root.nil?
+
+    if root.data > value
+      root.left = delete(value, root.left)
+    elsif root.data < value
+      root.right = delete(value, root.right)
+    else
+      return root.right if root.left.nil?
+
+      return root.left if root.right.nil?
+
+      successor = get_successor(root)
+      root.data = successor.data
+      root.right = delete(successor.data, root.right)
+    end
+    root
   end
 end
--- main.rb
+++ main.rb
@@ -4,4 +4,9 @@ require_relative 'lib/tree'
 
 test = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
+test.insert(2)
+test.pretty_print
+test.insert(6)
+test.pretty_print
+test.delete(2)
 test.pretty_print
